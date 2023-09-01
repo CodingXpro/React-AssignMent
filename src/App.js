dc9a93856863586bbae8f28f2e3d2ba8f1120fe8
Krishna Kumar <krishnassm420012@gmail.com>
2023-09-01 19:22:20 +0530

    Assignment-React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import Nav from './component/Nav'
-import Cart from './component/Cart'
-import './App.css'
-
+import {useEffect, useReducer} from 'react'
+import axios from 'axios'
+import './App.css';
+import Products from './component/Product';
+import Cart from './component/Cart';
+import { cartReducer } from './reducers/cartReducer';
 
 function App() {
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: [],
+    cart: [],
+  });
+
+  const fetchProducts = async()=>{
+    const {data} = await axios.get("https://dummyjson.com/products")
+
+    dispatch({
+      type: 'ADD_PRODUCTS',
+      payload: data.products
+    })
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+  
   return (
-    <div>
-        <Nav />
-        <Cart />
+
+    <div style={{display:'flex'}}>
+           
+       
+      <Products state={state} dispatch={dispatch} className="down1"/>
+      <Cart state={state} dispatch={dispatch} className="down2"/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
